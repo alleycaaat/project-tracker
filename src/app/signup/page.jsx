950e4425f377d5da591102716f15c6fb43e071b2
alleycaaat <achulslander@gmail.com>
2023-08-21 15:03:49 -0500

    add imports

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,3 +1,9 @@
+'use client'
+import Link from 'next/link';
+import React from 'react'
+import { useRouter } from 'next/navigation;';
+import { Axios } from 'axios';
+
 const signup = () => {
     return (
         <div>
